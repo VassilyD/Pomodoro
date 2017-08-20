402115b7270ca2ea9cb50bba4012b1a2b3a42e59
Vassily Dubois <vassily.dubois@gmail.com>
2017-08-20 21:14:42 +0200

    Ajout couleur changente progressBar, notification et son

--- js/core.js
+++ js/core.js
@@ -1,75 +1,63 @@
+$(document).ready(function(){
+	play = $('#play');
+	stop = $('#stop');
+	display = $('#time');
+	inputs = $('input');
+	staminaBar = $('#staminaBar');
+	inputs[0].value = Math.floor(tWork / MS_IN_MINUTE);;
+	inputs[1].value = Math.floor(tBreak / MS_IN_MINUTE);;
+	inputs[2].value = Math.floor(tStop / MS_IN_MINUTE);;
+	display.html(styleTime(tWork));
 
-	$(document).ready(function(){
-		play = $('#play');
-		stop = $('#stop');
-		display = $('#time');
-		inputs = $('input');
-		staminaBar = $('#staminaBar');
-		inputs[0].value = Math.floor(tWork / MS_IN_MINUTE);;
-		inputs[1].value = Math.floor(tBreak / MS_IN_MINUTE);;
-		inputs[2].value = Math.floor(tStop / MS_IN_MINUTE);;
-		display.html(styleTime(tWork));
 
-		play.click(function(){
-			if(!isLooping) {
-				dateFinChrono = Date.now() + tWork;
-				timing();
-				play.text('Pause');
-				timer = setTimeout(launchTimer, tempsRestant % MS_IN_SECONDE);
-				isLooping = true;
-			}
-			else if(isPaused) {
-				dateFinChrono = Date.now() + tempsRestant;
-				timing();
-				timer = setTimeout(launchTimer, tempsRestant % MS_IN_SECONDE);
-				isPaused = false;
-				play.text('Pause');
-			}
-			else {
-				clearInterval(timer);
-				timing();
-				isPaused = true;
-				play.text('Play');
-			}
+	if (!('Notification' in window)){
+		alert('This browser don\'t support Desktop Notifications.');
+	} else if (Notification.permission !== 'denied'){
+		Notification.requestPermission(function(permission){
+			Notification.permission = permission;
 		});
+	}
 
-		stop.click(function(){
-			clearInterval(timer);
-			isLooping = isPaused = inBreak = false;
-			tempsRestant = cycle = dateFinChrono = 0;
-			display.html(styleTime(0));
-			staminaBar.css('width', '100%');
-			play.text('Lancer');
-		});
-		
-		$('input:first').change(function(){
-			if(!inBreak && isLooping) {
-				if(isPaused) dateFinChrono = Date.now() + tempsRestant;
-				dateFinChrono += inputs[0].value * MS_IN_MINUTE - tWork;
-				tWork = inputs[0].value * MS_IN_MINUTE;
-				timing();
-			}
-			else {
-				tWork = inputs[0].value * MS_IN_MINUTE;
-				if(!inBreak) display.html(styleTime(tWork));
-			}
-		});
-		$('input:nth-child(3)').change(function(){
-			if(inBreak && cycle < 3) {
-				if(isPaused) dateFinChrono = Date.now() + tempsRestant;
-				dateFinChrono += inputs[1].value * MS_IN_MINUTE - tBreak;
-				tBreak = inputs[1].value * MS_IN_MINUTE;
-				timing();
-			}
-			else tBreak = inputs[1].value * MS_IN_MINUTE;
-		});
-		$('input:last').change(function(){
-			if(inBreak && cycle == 3) {
-				if(isPaused) dateFinChrono = Date.now() + tempsRestant;
-				dateFinChrono += inputs[2].value * MS_IN_MINUTE - tStop;
-				tStop = inputs[2].value * MS_IN_MINUTE;
-				timing();
-			}
-			else tStop = inputs[2].value * MS_IN_MINUTE;
-		});
+
+	play.click(switchPlay);
+
+	stop.click(stopTimer);
+	
+	$('input:first').change(function(){
+		if(!inBreak && isLooping) {
+			if(isPaused) dateFinChrono = Date.now() + tempsRestant;
+			dateFinChrono += inputs[0].value * MS_IN_MINUTE - tWork;
+			tWork = inputs[0].value * MS_IN_MINUTE;
+			timing();
+		}
+		else {
+			tWork = inputs[0].value * MS_IN_MINUTE;
+			if(!inBreak) display.html(styleTime(tWork));
+		}
 	});
+	$('input:nth-child(3)').change(function(){
+		if(inBreak && cycle < 3) {
+			if(isPaused) dateFinChrono = Date.now() + tempsRestant;
+			dateFinChrono += inputs[1].value * MS_IN_MINUTE - tBreak;
+			tBreak = inputs[1].value * MS_IN_MINUTE;
+			timing();
+		}
+		else tBreak = inputs[1].value * MS_IN_MINUTE;
+	});
+	$('input:last').change(function(){
+		if(inBreak && cycle == 3) {
+			if(isPaused) dateFinChrono = Date.now() + tempsRestant;
+			dateFinChrono += inputs[2].value * MS_IN_MINUTE - tStop;
+			tStop = inputs[2].value * MS_IN_MINUTE;
+			timing();
+		}
+		else tStop = inputs[2].value * MS_IN_MINUTE;
+	});
+
+	$('body').keypress(function(event){
+		if(event.which == 32) {
+			clearInterval(timer);
+			switchPlay();
+		}
+	});
+});
--- js/function.js
+++ js/function.js
@@ -6,6 +6,7 @@ const MS_IN_MINUTE = 60 * MS_IN_SECONDE;
 let isLooping = isPaused = false;
 let dateFinChrono = 0;
 let tempsRestant = 0;
+let timer = 0;
 let play = {};
 let stop = {};
 let inputs = {};
@@ -16,6 +17,41 @@ let tBreak = 10 * MS_IN_MINUTE;
 let tStop = 30 * MS_IN_MINUTE;
 let inBreak = false;
 let cycle = 0;
+let notification;
+let alertAudio = new Audio('audio/alert.wav');
+
+
+function switchPlay(){
+	if(!isLooping) {
+		dateFinChrono = Date.now() + tWork;
+		timing();
+		play.text('Pause');
+		timer = setTimeout(launchTimer, tempsRestant % MS_IN_SECONDE);
+		isLooping = true;
+	}
+	else if(isPaused) {
+		dateFinChrono = Date.now() + tempsRestant;
+		timing();
+		timer = setTimeout(launchTimer, tempsRestant % MS_IN_SECONDE);
+		isPaused = false;
+		play.text('Pause');
+	}
+	else {
+		clearInterval(timer);
+		timing();
+		isPaused = true;
+		play.text('Play');
+	}
+}
+
+function stopTimer(){
+	clearInterval(timer);
+	isLooping = isPaused = inBreak = false;
+	tempsRestant = cycle = dateFinChrono = 0;
+	display.html(styleTime(0));
+	staminaBar.css('width', '100%');
+	play.text('Lancer');
+}
 
 function launchTimer() {
 	tempsRestant = (dateFinChrono - Date.now());
@@ -25,13 +61,12 @@ function launchTimer() {
 
 function styleTime(time = 0) {
 	var minutes = Math.floor(time / MS_IN_MINUTE);
-	if(minutes < 10 && minutes > 0) minutes = "0" + minutes;
-	else if(minutes <= 0) minutes = "00";
-	time -= minutes * MS_IN_MINUTE;
+	minutes = (minutes<0)?0:minutes;
+	minutes = ('0' + minutes).slice(-2);
 
-	var secondes = Math.floor(time / MS_IN_SECONDE);
-	if(secondes < 10 && secondes > 0) secondes = "0" + secondes;
-	else if(secondes <= 0) secondes = "00";
+	var secondes = Math.floor((time % MS_IN_MINUTE) / MS_IN_SECONDE);
+	secondes = (secondes<0)?0:secondes;
+	secondes = ('0' + secondes).slice(-2);
 
 	return "" + minutes + " : " + secondes;
 }
@@ -39,25 +74,78 @@ function styleTime(time = 0) {
 function timing() {
 	// Q2 : Faut il déclarer mieux switchPhase(), qui n'est utile que dans timing(), ici ou bien en globale?
 	function switchPhase(newTime = 0) {
-		dateFinChrono = Date.now() + newTime;
+		tempsRestant = newTime;
 		display.html(styleTime(newTime));
 		inBreak = !inBreak;
+		//timer = setTimeout(launchTimer, newTime % MS_IN_SECONDE);
+		//isPaused = false;
+		//play.text('Pause');
 	}
 	tempsRestant = (dateFinChrono - Date.now());
 	display.html(styleTime(tempsRestant));
+	$('title').text(styleTime(tempsRestant) + ' Pomodoro');
+
 	var tmp = (!inBreak)?tWork:(cycle < 3)?tBreak:tStop;
 	tmp = 100 * tempsRestant / tmp;
 	tmp = (!isLooping || tmp > 100)?100:(tmp < 0)?0:tmp;
-	if (inBreak) staminaBar.animate({width: (100 - tmp)+'%'});
-	else staminaBar.animate({width: tmp+'%'});
+	if (inBreak) staminaBar.animate({width: (100 - tmp)+'%'}, 250);
+	else staminaBar.animate({width: tmp+'%'}, 250);
+
+	if(cycle == 3 && inBreak) staminaBar.css('background-color', 'rgb(' + (255*tmp/100) + ', ' + (128*(100-tmp)/100) + ', 0)');
+	else switch(cycle) {
+		case 0:
+			if(inBreak) staminaBar.css('background-color', 'rgb(128, 128, 0)');
+			else staminaBar.css('background-color', 'rgb(' + (128*(100-tmp)/100) + ', 128, 0)');
+			break;
+
+		case 1:
+			if(inBreak) staminaBar.css('background-color', 'rgb(255, 255, 0)');
+			else staminaBar.css('background-color', 'rgb(' + (128 + 128*(100-tmp)/100) + ', ' + (128 + 128*(100-tmp)/100) + ', 0)');
+			break;
+
+		case 2:
+			if(inBreak) staminaBar.css('background-color', 'rgb(255, 128, 0)');
+			else staminaBar.css('background-color', 'rgb(255, ' + (255 - 128*(100-tmp)/100) + ', 0)');
+			break;
+
+		case 3:
+			staminaBar.css('background-color', 'rgb(255, ' + (128 - 128*(100-tmp)/100) + ', 0)');
+			break;
+
+		default:
+			console.log('Ceci ne devrais pas arrivé!');
+	}
 
 	if(tempsRestant < 0) {
+		clearInterval(timer);
+		isPaused = true;
+		play.text('Play');
+		alertAudio.play();
 		if(inBreak) {
 			switchPhase(tWork)
 			if(cycle < 3) cycle++;
 			else cycle = 0;
+			notification = new Notification('Yohohoho!', {body: 'Il est temps de retourner au travail!'});
+			notification.onclick = function(){
+				switchPlay()
+				notification.close();
+			}
 		}
-		else if(cycle < 3) switchPhase(tBreak);
-		else switchPhase(tStop);
+		else if(cycle < 3) {
+			switchPhase(tBreak)
+			var notification = new Notification('Yohohoho!', {body: 'Il est temps de se détendre ' + Math.floor(tBreak / MS_IN_MINUTE) + 'mn!'});
+			notification.onclick = function(){
+				switchPlay()
+				notification.close();
+			}
+		}
+		else {
+			switchPhase(tStop)
+			var notification = new Notification('Yohohoho!', {body: 'Il est temps de déconnecter!!!'});
+			notification.onclick = function(){
+				switchPlay()
+				notification.close();
+			}
+		}
 	}
 }
